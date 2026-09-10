8d585c676a555d363962eeb45c2de4633f175112
krishna pratap patel <krishnap32422@gmail.com>
2026-09-10 21:52:22 +0530

    Allow production frontend in CORS

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ app=FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173","https://hrms-management-system-1-ejrd.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
